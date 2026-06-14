9f885eeb85e7ac0dbe1c9f76223c309edcb2b7eb
Samuel Appiah Kojo <57666310+SAMMYOUN@users.noreply.github.com>
2026-06-14 21:06:09 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        darkBg: '#000000',
+                        cardBgDark: '#1c1c1e',
+                        cardBgLight: '#ffffff',
+                        appleGray: '#8e8e93',
+                        techBlue: '#007aff',
+                        innovGreen: '#34c759',
+                    }
+                }
+            }
+        }
+    
